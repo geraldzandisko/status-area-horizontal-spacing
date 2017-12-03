e7bee4a4081c4ec2965cb31e81dc3c559b40fbd4
Paolo Inaudi <p91paul@gmail.com>
2017-12-03 10:49:25 +0100

    Shell buildforupload

---
+++ buildforupload.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm -rf status-area-horizontal-spacing@mathematical.coffee.gmail.com.zip
+cd status-area-horizontal-spacing@mathematical.coffee.gmail.com/
+zip -r ../status-area-horizontal-spacing@mathematical.coffee.gmail.com.zip *
