08545a76f4095df6290be51c3a99e1c0ffafe793
Paolo Inaudi <p91paul@gmail.com>
2019-11-10 11:25:00 +0100

    Fix build script

--- buildforupload.sh
+++ buildforupload.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-rm -rf status-area-horizontal-spacing@mathematical.coffee.gmail.com.zip
-cd status-area-horizontal-spacing@mathematical.coffee.gmail.com/
-zip -r ../status-area-horizontal-spacing@mathematical.coffee.gmail.com.zip *
+#rm -rf status-area-horizontal-spacing@mathematical.coffee.gmail.com.zip
+#cd status-area-horizontal-spacing@mathematical.coffee.gmail.com/
+#zip -r ../status-area-horizontal-spacing@mathematical.coffee.gmail.com.zip *
+make zip
